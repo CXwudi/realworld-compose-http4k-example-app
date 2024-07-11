fdeecbc8439f1f0a02e6e305d11ea2c6229eccb4
CXwudi <cx2010017@gmail.com>
2024-07-11 19:59:12 -0400

    🔧 Solve a bug of incorrect Flow usage causing blocking
    This commit simplifies the UserConfigService implementation by replacing the generic setSingleConfig method with specific methods for updating URL, username, and token. It also updates the corresponding SQL queries to perform individual updates.

--- conduit-frontend/frontend-decompose-logic/src/commonMain/kotlin/mikufan/cx/conduit/frontend/logic/service/UserConfigService.kt
+++ conduit-frontend/frontend-decompose-logic/src/commonMain/kotlin/mikufan/cx/conduit/frontend/logic/service/UserConfigService.kt
@@ -8,10 +8,10 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.single
 import kotlinx.coroutines.flow.stateIn
 import migrations.UserConfig
 import mikufan.cx.conduit.frontend.logic.repo.db.UserConfigQueries
+import org.lighthousegames.logging.logging
 
 
 interface UserConfigService {
@@ -48,29 +48,19 @@ class UserConfigServiceImpl(
 
   override suspend fun setUrl(url: String?) {
     if (url != null) {
-      setSingleConfig(url = url)
+      userConfigDao.setUrl(url)
     } else {
       throw IllegalArgumentException("Invalid URL format, please check and try again")
     }
   }
 
   override suspend fun setUsername(username: String?) {
-    setSingleConfig(username = username)
+    userConfigDao.setUsername(username)
   }
 
   override suspend fun setToken(token: String?) {
-    setSingleConfig(token = token)
-  }
-
-  internal suspend fun setSingleConfig(
-    url: String? = null,
-    username: String? = null,
-    token: String? = null,
-  ) {
-    val single = userConfigFlow.single()
-    val newUrl = url ?: single.url
-    val newUsername = username ?: single.username
-    val newToken = token ?: single.token
-    userConfigDao.setSingleConfig(url = newUrl, username = newUsername, token = newToken)
+    userConfigDao.setToken(token)
   }
 }
+
+private val log = logging()
