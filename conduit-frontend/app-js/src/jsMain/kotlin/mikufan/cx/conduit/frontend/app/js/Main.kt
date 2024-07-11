6256f4e19788fb901c9dff19c2c4999e20e641ed
CXwudi <cx2010017@gmail.com>
2024-07-11 18:17:19 -0400

    🚀 Initialize SqlDriver within global coroutine
    This commit updates the initialization process of the Koin application and SqlDriver. It wraps the initialization in a global coroutine to ensure proper setup of the SqlDriver before the main UI is rendered.

--- conduit-frontend/app-js/src/jsMain/kotlin/mikufan/cx/conduit/frontend/app/js/Main.kt
+++ conduit-frontend/app-js/src/jsMain/kotlin/mikufan/cx/conduit/frontend/app/js/Main.kt
@@ -4,22 +4,33 @@ import com.arkivanov.decompose.DefaultComponentContext
 import com.arkivanov.essenty.lifecycle.LifecycleRegistry
 import com.arkivanov.essenty.lifecycle.resume
 import com.arkivanov.essenty.lifecycle.stop
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import mikufan.cx.conduit.frontend.logic.allModules
 import mikufan.cx.conduit.frontend.logic.component.DefaultRootNavComponent
 import mikufan.cx.conduit.frontend.logic.component.util.toLocalKoinComponent
+import mikufan.cx.conduit.frontend.logic.repo.db.AppDb
+import mikufan.cx.conduit.frontend.logic.repo.db.provideDbDriver
 import mikufan.cx.conduit.frontend.ui.MainUI
 import org.jetbrains.skiko.wasm.onWasmReady
+import org.koin.core.KoinApplication
 import org.koin.dsl.koinApplication
 import web.dom.DocumentVisibilityState
 import web.dom.document
 import web.events.EventType
 import web.events.addEventListener
 
-fun initKoin() = koinApplication {
-  modules(allModules)
+suspend fun initKoin(): KoinApplication {
+  val application = koinApplication {
+    modules(allModules)
+  }
+  application.koin.declare(provideDbDriver(AppDb.Schema))
+  return application
 }
 
 
+@OptIn(DelicateCoroutinesApi::class)
 fun main(args: Array<String>) {
 
   val lifecycle = LifecycleRegistry()
@@ -27,16 +38,18 @@ fun main(args: Array<String>) {
     lifecycle = lifecycle
   )
 
-  val koin = initKoin().koin
-  val rootComponent = DefaultRootNavComponent(defaultComponentContext, koin.toLocalKoinComponent())
-
   lifecycle.attachToDocument()
+  GlobalScope.launch { // initialize within a global coroutine, workaround to initialize SqlDriver from koin
+    val koin = initKoin().koin
+    val rootComponent = DefaultRootNavComponent(defaultComponentContext, koin.toLocalKoinComponent(), koin.get())
 
-  onWasmReady {
-    BrowserViewportWindow(title = "Conduit Web", canvasElementId = "ConduitCanvas") {
-      MainUI(koin, rootComponent)
+    onWasmReady {
+      BrowserViewportWindow(title = "Conduit Web", canvasElementId = "ConduitCanvas") {
+        MainUI(koin, rootComponent)
+      }
     }
   }
+
 }
 
 private fun LifecycleRegistry.attachToDocument() {
