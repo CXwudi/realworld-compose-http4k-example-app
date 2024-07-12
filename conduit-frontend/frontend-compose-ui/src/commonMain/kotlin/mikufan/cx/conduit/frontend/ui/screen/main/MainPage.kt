6ab17c8de6f6e4d27eb6e99e2102fab909604be6
CXwudi <cx2010017@gmail.com>
2024-07-11 21:36:30 -0400

    fix main page overlay in edge to edge

--- conduit-frontend/frontend-compose-ui/src/commonMain/kotlin/mikufan/cx/conduit/frontend/ui/screen/main/MainPage.kt
+++ conduit-frontend/frontend-compose-ui/src/commonMain/kotlin/mikufan/cx/conduit/frontend/ui/screen/main/MainPage.kt
@@ -1,12 +1,16 @@
 package mikufan.cx.conduit.frontend.ui.screen.main
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
 fun MainPage() {
-  Column {
+  Column(modifier = Modifier.windowInsetsPadding(WindowInsets.systemBars)) {
     Text("TODO: main page")
   }
 }
