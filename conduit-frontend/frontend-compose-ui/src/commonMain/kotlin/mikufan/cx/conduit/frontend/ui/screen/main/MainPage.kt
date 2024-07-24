7d5d18df1772f090b5bd541472554fb4356fa83f
CXwudi <cx2010017@gmail.com>
2024-07-23 22:03:08 -0400

    We decided not to integrate MVIKotlin and Decompose, see #9 comment

--- conduit-frontend/frontend-compose-ui/src/commonMain/kotlin/mikufan/cx/conduit/frontend/ui/screen/main/MainPage.kt
+++ conduit-frontend/frontend-compose-ui/src/commonMain/kotlin/mikufan/cx/conduit/frontend/ui/screen/main/MainPage.kt
@@ -3,21 +3,17 @@ package mikufan.cx.conduit.frontend.ui.screen.main
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import com.arkivanov.decompose.extensions.compose.subscribeAsState
 import mikufan.cx.conduit.frontend.logic.component.main.MainNavComponent
 
 @Composable
 fun MainPage(component: MainNavComponent, modifier: Modifier = Modifier) {
 
-  val mainNavState by component.state.collectAsState()
-  val mainNavManuItems by remember { derivedStateOf { mainNavState.menuItems } }
+  val mainNavState by component.state.subscribeAsState()
 
   Column {
     Text("TODO Menus:")
-    Text("$mainNavManuItems")
   }
 }
--- conduit-frontend/frontend-decompose-logic/src/commonMain/kotlin/mikufan/cx/conduit/frontend/logic/component/main/MainNavComponent.kt
+++ conduit-frontend/frontend-decompose-logic/src/commonMain/kotlin/mikufan/cx/conduit/frontend/logic/component/main/MainNavComponent.kt
@@ -4,21 +4,22 @@ import com.arkivanov.decompose.ComponentContext
 import com.arkivanov.decompose.router.slot.ChildSlot
 import com.arkivanov.decompose.router.slot.SlotNavigation
 import com.arkivanov.decompose.router.slot.childSlot
+import com.arkivanov.decompose.value.MutableValue
 import com.arkivanov.decompose.value.Value
 import com.arkivanov.essenty.lifecycle.coroutines.coroutineScope
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import kotlinx.serialization.Serializable
 import mikufan.cx.conduit.frontend.logic.component.util.LocalKoinComponent
-import mikufan.cx.conduit.frontend.logic.component.util.asValue
 import mikufan.cx.conduit.frontend.logic.service.UserConfigService
 import mikufan.cx.conduit.frontend.logic.service.UserConfigState
 
 interface MainNavComponent {
   val childSlot: Value<ChildSlot<*, MainNavComponentChild>>
   val state: Value<MainNavState>
+
+  fun send(intent: MainNavIntent)
 }
 
 sealed interface MainNavComponentChild {
@@ -32,8 +33,37 @@ sealed interface MainNavComponentChild {
 }
 
 data class MainNavState(
-  val menuItems: List<MainNavMenuItem>
+  val mode: MainNavMode
 )
+
+enum class MainNavMode(
+  val menuItems: List<MainNavMenuItem>,
+) {
+  NOT_LOGGED_IN(
+    listOf(
+      MainNavMenuItem.Feed,
+      MainNavMenuItem.SignInUp
+    )
+  ),
+  LOGGED_IN(
+    listOf(
+      MainNavMenuItem.Feed,
+      MainNavMenuItem.Favourite,
+      MainNavMenuItem.Me
+    )
+  ),
+}
+
+sealed interface MainNavIntent {
+
+  data object ToSignInMode: MainNavIntent
+  data object ToLogoutMode: MainNavIntent
+
+  data object ToFeedPage: MainNavIntent
+  data object ToFavouritePage: MainNavIntent
+  data object ToMePage: MainNavIntent
+  data object ToSignInUpPage: MainNavIntent
+}
 
 enum class MainNavMenuItem(
   val menuName: String,
@@ -49,19 +79,6 @@ class DefaultMainNavComponent(
   private val koin: LocalKoinComponent,
   private val userConfigService: UserConfigService,
 ) : MainNavComponent, ComponentContext by componentContext {
-
-  companion object {
-    private val unloginMenuItems = listOf(
-      MainNavMenuItem.Feed,
-      MainNavMenuItem.SignInUp
-    )
-
-    private val loginMenuItems = listOf(
-      MainNavMenuItem.Feed,
-      MainNavMenuItem.Favourite,
-      MainNavMenuItem.Me
-    )
-  }
 
   private val slotNavigation = SlotNavigation<Config>()
 
@@ -83,14 +100,21 @@ class DefaultMainNavComponent(
     Config.SignInUp -> MainNavComponentChild.SignInUp
   }
 
-  override val state: Value<MainNavState> =
+  private val _state = MutableValue(MainNavState(MainNavMode.NOT_LOGGED_IN))
+  override val state: Value<MainNavState> get() = _state
+
+  init {
+    setupLoginStatusChange()
+  }
+
+  private fun setupLoginStatusChange() = coroutineScope().launch {
     userConfigService.userConfigStateFlow.map { userConfigState ->
-      val listOfMenuItem = when (userConfigState) {
+      when (userConfigState) {
         is UserConfigState.Loaded -> {
           if (userConfigState.token.isNullOrBlank()) {
-            unloginMenuItems
+            MainNavIntent.ToLogoutMode
           } else {
-            loginMenuItems
+            MainNavIntent.ToSignInMode
           }
         }
 
@@ -98,14 +122,20 @@ class DefaultMainNavComponent(
           error("Should not happen since the main page appears only after the user config is loaded")
         }
       }
-      MainNavState(listOfMenuItem)
     }
       .distinctUntilChanged()
-      .stateIn(
-        coroutineScope(),
-        SharingStarted.Eagerly,
-        MainNavState(unloginMenuItems)
-      ).asValue(coroutineScope())
+      .collect {
+        send(it)
+      }
+  }
+
+  override fun send(intent: MainNavIntent) {
+    when(intent) {
+      is MainNavIntent.ToSignInMode -> _state.value = MainNavState(MainNavMode.LOGGED_IN)
+      is MainNavIntent.ToLogoutMode -> _state.value = MainNavState(MainNavMode.NOT_LOGGED_IN)
+      else -> TODO()
+    }
+  }
 
   @Serializable
   sealed interface Config {
