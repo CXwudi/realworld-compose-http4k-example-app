893b09472ed9d9f039006ce9feb65a35771ae0b5
CXwudi <cx2010017@gmail.com>
2024-07-25 17:59:01 -0400

    ✨ Implement bidirectional conversion between StateFlow and Value

--- conduit-frontend/frontend-decompose-logic/src/commonMain/kotlin/mikufan/cx/conduit/frontend/logic/component/util/StateFlowUtil.kt
+++ conduit-frontend/frontend-decompose-logic/src/commonMain/kotlin/mikufan/cx/conduit/frontend/logic/component/util/StateFlowUtil.kt
@@ -3,25 +3,60 @@ package mikufan.cx.conduit.frontend.logic.component.util
 import com.arkivanov.decompose.Cancellation
 import com.arkivanov.decompose.value.Value
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 
 
-fun <T : Any> StateFlow<T>.asValue(scope: CoroutineScope = CoroutineScope(Dispatchers.Default)): Value<T> =
-  object : Value<T>() {
-    override val value: T
-      get() = this@asValue.value
-
-    override fun subscribe(observer: (T) -> Unit): Cancellation {
-
-      val job = this@asValue
-        .onEach { observer(it) }
-        .launchIn(scope)
-      return Cancellation {
-        job.cancel()
-      }
-    }
-
+fun <T : Any> StateFlow<T>.asValue(scope: CoroutineScope): Value<T> =
+  if (this is ValueStateFlow) {
+    this.source
+  } else {
+    StateFlowValue(this, scope)
   }
+
+private class StateFlowValue<out T : Any>(
+  val source: StateFlow<T>,
+  private val scope: CoroutineScope
+) : Value<T>() {
+  override val value: T
+    get() = source.value
+
+  override fun subscribe(observer: (T) -> Unit): Cancellation {
+    val job = source
+      .onEach { observer(it) }
+      .launchIn(scope)
+    return Cancellation {
+      job.cancel()
+    }
+  }
+}
+
+fun <T : Any> Value<T>.asStateFlow(): StateFlow<T> =
+  if (this is StateFlowValue) {
+    this.source
+  } else {
+    ValueStateFlow(this)
+  }
+
+private class ValueStateFlow<out T : Any>(val source: Value<T>) : StateFlow<T> {
+
+  override val value: T
+    get() = source.value
+
+  override val replayCache: List<T>
+    get() = listOf(source.value)
+
+  override suspend fun collect(collector: FlowCollector<T>): Nothing {
+    val flow = MutableStateFlow(source.value)
+    val disposable = source.subscribe { flow.value = it }
+
+    try {
+      flow.collect(collector)
+    } finally {
+      disposable.cancel()
+    }
+  }
+}
