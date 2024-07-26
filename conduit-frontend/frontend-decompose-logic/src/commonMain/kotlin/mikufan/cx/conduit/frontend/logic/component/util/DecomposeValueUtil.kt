300d48f7bf58e0274063d74063bf03a67e880897
CXwudi <cx2010017@gmail.com>
2024-07-26 15:39:02 -0400

    ✨ Add DecomposeValueUtil for Value and StateFlow conversions

--- conduit-frontend/frontend-decompose-logic/src/commonMain/kotlin/mikufan/cx/conduit/frontend/logic/component/util/DecomposeValueUtil.kt
+++ conduit-frontend/frontend-decompose-logic/src/commonMain/kotlin/mikufan/cx/conduit/frontend/logic/component/util/DecomposeValueUtil.kt
@@ -2,16 +2,21 @@ package mikufan.cx.conduit.frontend.logic.component.util
 
 import com.arkivanov.decompose.Cancellation
 import com.arkivanov.decompose.value.Value
+import com.arkivanov.mvikotlin.core.rx.observer
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.stateIn
 
 
-fun <T : Any> StateFlow<T>.asValue(scope: CoroutineScope): Value<T> =
+fun <T : Any> StateFlow<T>.toValue(scope: CoroutineScope): Value<T> =
   if (this is ValueStateFlow) {
     this.source
   } else {
@@ -36,7 +41,7 @@ private class StateFlowValue<out T : Any>(
 }
 
 @ExperimentalCoroutinesApi
-fun <T : Any> Value<T>.asStateFlow(): StateFlow<T> =
+fun <T : Any> Value<T>.toStateFlow(): StateFlow<T> =
   if (this is StateFlowValue) {
     this.source
   } else {
@@ -62,3 +67,18 @@ private class ValueStateFlow<out T : Any>(val source: Value<T>) : StateFlow<T> {
     }
   }
 }
+
+fun <T : Any> Value<T>.toFlow() = callbackFlow {
+  val disposable = this@toFlow.subscribe {
+    observer<T>(
+      onComplete = { channel.close() },
+      onNext = { channel.trySend(it) }
+    )
+  }
+  awaitClose { disposable.cancel() }
+}
+
+fun <T : Any> Value<T>.toStateFlow(
+  scope: CoroutineScope,
+  started: SharingStarted = SharingStarted.Eagerly,
+): StateFlow<T> = toFlow().stateIn(scope, started, this.value)
