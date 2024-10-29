95197178081a5fb675a58a681742e68440e33986
CXwudi <cx2010017@gmail.com>
2024-10-29 18:21:17 -0400

    ✨ Add login endpoint to AuthApi

--- conduit-frontend/frontend-decompose-logic/src/commonMain/kotlin/mikufan/cx/conduit/frontend/logic/repo/api/AuthApi.kt
+++ conduit-frontend/frontend-decompose-logic/src/commonMain/kotlin/mikufan/cx/conduit/frontend/logic/repo/api/AuthApi.kt
@@ -2,12 +2,21 @@ package mikufan.cx.conduit.frontend.logic.repo.api
 
 import de.jensklingenberg.ktorfit.http.Body
 import de.jensklingenberg.ktorfit.http.POST
-import mikufan.cx.conduit.common.NewUserReq
+import io.ktor.client.statement.HttpResponse
+import mikufan.cx.conduit.common.UserLoginDto
+import mikufan.cx.conduit.common.UserRegisterDto
+import mikufan.cx.conduit.common.UserReq
 import mikufan.cx.conduit.common.UserRsp
 
 interface AuthApi {
   @POST("users")
   suspend fun register(
-    @Body body: NewUserReq
+    @Body body: UserReq<UserRegisterDto>
   ): UserRsp
+
+  @POST("users/login")
+  suspend fun login(
+    @Body body: UserReq<UserLoginDto>
+  ): HttpResponse
+
 }
