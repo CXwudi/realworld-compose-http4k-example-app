e824efc029c8102da1975566a0ffe313b25afc19
CXwudi <cx2010017@gmail.com>
2024-10-29 18:19:08 -0400

    🔨 refactor user DTOs for better reusability
    Introduce generic UserReq, separate UserLoginDto and UserRegisterDto, and add utility functions for creating request objects

--- conduit-common/src/commonMain/kotlin/mikufan/cx/conduit/common/user.kt
+++ conduit-common/src/commonMain/kotlin/mikufan/cx/conduit/common/user.kt
@@ -2,35 +2,51 @@ package mikufan.cx.conduit.common
 
 import kotlinx.serialization.Serializable
 
-
 @Serializable
-data class NewUserReq(
-  val user: NewUserDto
+data class UserReq<T>(
+  val user: T
 )
 
 @Serializable
-data class NewUserDto(
+data class UserRegisterDto(
   val email: String,
   val username: String,
   val password: String
 )
 
-fun NewUserReq(
-  email: String,
-  username: String,
-  password: String
-) = NewUserReq(NewUserDto(email, username, password))
+@Serializable
+data class UserLoginDto(
+  val email: String,
+  val password: String
+)
 
+object UserDtoUtils {
+  fun createLoginReq(email: String, password: String) = UserReq(
+    UserLoginDto(
+      email = email,
+      password = password
+    )
+  )
 
+  fun createRegisterReq(email: String, username: String, password: String) = UserReq(
+    UserRegisterDto(
+      email = email,
+      username = username,
+      password = password
+    )
+  )
+}
 
 @Serializable
 data class UserRsp(
   val user: UserDto
 )
 
+/**
+ * The user response DTO for login and register
+ */
 @Serializable
 data class UserDto(
-  val id: Int,
   val email: String,
   val username: String,
   val bio: String?,
