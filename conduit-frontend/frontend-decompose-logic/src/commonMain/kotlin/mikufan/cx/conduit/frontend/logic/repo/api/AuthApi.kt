27d4fd444da345429f14cd67374a8f0a33967995
CXwudi <cx2010017@gmail.com>
2024-10-29 20:17:48 -0400

    ✨ Update AuthApi to use ConduitResponse wrapper type
    Refactor API endpoints to use a custom response type for better error handling and response state management

--- conduit-frontend/frontend-decompose-logic/src/commonMain/kotlin/mikufan/cx/conduit/frontend/logic/repo/api/AuthApi.kt
+++ conduit-frontend/frontend-decompose-logic/src/commonMain/kotlin/mikufan/cx/conduit/frontend/logic/repo/api/AuthApi.kt
@@ -2,21 +2,21 @@ package mikufan.cx.conduit.frontend.logic.repo.api
 
 import de.jensklingenberg.ktorfit.http.Body
 import de.jensklingenberg.ktorfit.http.POST
-import io.ktor.client.statement.HttpResponse
 import mikufan.cx.conduit.common.UserLoginDto
 import mikufan.cx.conduit.common.UserRegisterDto
 import mikufan.cx.conduit.common.UserReq
 import mikufan.cx.conduit.common.UserRsp
+import mikufan.cx.conduit.frontend.logic.repo.api.util.ConduitResponse
 
 interface AuthApi {
   @POST("users")
   suspend fun register(
     @Body body: UserReq<UserRegisterDto>
-  ): UserRsp
+  ): ConduitResponse<UserRsp>
 
   @POST("users/login")
   suspend fun login(
     @Body body: UserReq<UserLoginDto>
-  ): HttpResponse
+  ): ConduitResponse<UserRsp>
 
 }
