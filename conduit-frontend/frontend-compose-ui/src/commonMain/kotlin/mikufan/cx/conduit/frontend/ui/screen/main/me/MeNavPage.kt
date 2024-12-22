c3500ac5843a2125f9ee2a3a7d847f348e6da417
CXwudi <cx2010017@gmail.com>
2024-12-21 23:42:56 -0500

    ♻️ refactor MeNavPage to accept MePageComponent

--- conduit-frontend/frontend-compose-ui/src/commonMain/kotlin/mikufan/cx/conduit/frontend/ui/screen/main/me/MeNavPage.kt
+++ conduit-frontend/frontend-compose-ui/src/commonMain/kotlin/mikufan/cx/conduit/frontend/ui/screen/main/me/MeNavPage.kt
@@ -19,7 +19,7 @@ fun MeNavPage(meNavComponent: MeNavComponent, modifier: Modifier = Modifier) {
     modifier = modifier,
     content = { child ->
       when (child) {
-        is MeNavComponentChild.RootMeComponent -> MePage()
+        is MeNavComponentChild.MePage -> MePage(child.mePageComponent)
       }
     }
   )
