97d082ecfa7368b4be402193d7d3a1c0a7a4cf55
CXwudi <cx2010017@gmail.com>
2025-01-11 18:19:28 -0500

    📦️ Wrap article list API response with custom type
    Change the return type of the article list API from `ArticlesRsp` to `ConduitResponse<ArticlesRsp>` for better error handling and response management.

--- conduit-frontend/frontend-decompose-logic/src/commonMain/kotlin/mikufan/cx/conduit/frontend/logic/repo/api/ArticleApi.kt
+++ conduit-frontend/frontend-decompose-logic/src/commonMain/kotlin/mikufan/cx/conduit/frontend/logic/repo/api/ArticleApi.kt
@@ -3,6 +3,7 @@ package mikufan.cx.conduit.frontend.logic.repo.api
 import de.jensklingenberg.ktorfit.http.GET
 import de.jensklingenberg.ktorfit.http.Query
 import mikufan.cx.conduit.common.ArticlesRsp
+import mikufan.cx.conduit.frontend.logic.repo.api.util.ConduitResponse
 
 interface ArticleApi {
   @GET("articles")
@@ -12,5 +13,5 @@ interface ArticleApi {
     @Query favorited: String? = null,
     @Query limit: Int? = null,
     @Query offset: Int? = null
-  ): ArticlesRsp
+  ): ConduitResponse<ArticlesRsp>
 }
