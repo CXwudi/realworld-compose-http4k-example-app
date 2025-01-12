f12cc95d1d5cbc7431cf7f0f66199c56d56f1761
CXwudi <cx2010017@gmail.com>
2025-01-11 23:05:26 -0500

    🐛 Fix compilation error by implementing KoinStartup interface correctly
    Update MainApplication to properly implement KoinStartup and move Koin configuration to onKoinStartup method

--- conduit-frontend/app-android/src/androidMain/kotlin/mikufan/cx/conduit/frontend/app/android/MainApplication.kt
+++ conduit-frontend/app-android/src/androidMain/kotlin/mikufan/cx/conduit/frontend/app/android/MainApplication.kt
@@ -5,20 +5,20 @@ import mikufan.cx.conduit.frontend.logic.allModules
 import org.koin.android.ext.koin.androidContext
 import org.koin.androix.startup.KoinStartup
 import org.koin.core.annotation.KoinExperimentalAPI
+import org.koin.dsl.KoinConfiguration
+import org.koin.dsl.koinConfiguration
 
 @KoinExperimentalAPI
-class MainApplication : android.app.Application() {
-
-  init {
-    KoinStartup.onKoinStartup {
-      androidContext(this@MainApplication)
-      modules(allModules)
-    }
-  }
+class MainApplication : android.app.Application(), KoinStartup {
 
   override fun onCreate() {
     super.onCreate()
     log.info { "onCreate" }
+  }
+
+  override fun onKoinStartup(): KoinConfiguration = koinConfiguration {
+    androidContext(this@MainApplication)
+    modules(allModules)
   }
 
 }
