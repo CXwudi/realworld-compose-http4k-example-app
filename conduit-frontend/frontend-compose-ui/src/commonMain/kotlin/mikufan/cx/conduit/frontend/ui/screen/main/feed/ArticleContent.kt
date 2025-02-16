b94d0af7320ec853108140fc48151edd5398ac88
CXwudi <cx2010017@gmail.com>
2025-02-16 16:05:08 -0500

    ✨ feat: implement adaptive panel mode for ArticlesListDetailPanel
    Add adaptive panel mode based on window width, with single/dual panel switching and animation. Using default horizontal layout for now.

---
+++ conduit-frontend/frontend-compose-ui/src/commonMain/kotlin/mikufan/cx/conduit/frontend/ui/screen/main/feed/ArticleContent.kt
@@ -0,0 +1,10 @@
+package mikufan.cx.conduit.frontend.ui.screen.main.feed
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun ArticleContent() {
+  Text("ArticleContent")
+}
--- conduit-frontend/frontend-compose-ui/src/commonMain/kotlin/mikufan/cx/conduit/frontend/ui/screen/main/feed/ArticlesListDetailPanel.kt
+++ conduit-frontend/frontend-compose-ui/src/commonMain/kotlin/mikufan/cx/conduit/frontend/ui/screen/main/feed/ArticlesListDetailPanel.kt
@@ -1,13 +1,48 @@
 package mikufan.cx.conduit.frontend.ui.screen.main.feed
 
-import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.window.core.layout.WindowWidthSizeClass
 import com.arkivanov.decompose.ExperimentalDecomposeApi
+import com.arkivanov.decompose.extensions.compose.experimental.panels.ChildPanels
+import com.arkivanov.decompose.extensions.compose.experimental.panels.ChildPanelsAnimators
+import com.arkivanov.decompose.extensions.compose.experimental.stack.animation.fade
+import com.arkivanov.decompose.extensions.compose.experimental.stack.animation.plus
+import com.arkivanov.decompose.extensions.compose.experimental.stack.animation.scale
+import com.arkivanov.decompose.router.panels.ChildPanelsMode
 import mikufan.cx.conduit.frontend.logic.component.main.feed.ArticlesListDetailNavComponent
 
 
 @OptIn(ExperimentalDecomposeApi::class)
 @Composable
 fun ArticlesListDetailPanel(component: ArticlesListDetailNavComponent) {
-  Text("ArticlesListDetailPanel")
+  val mode = getPanelMode()
+  LaunchedEffect(mode) {
+    component.setMode(mode)
+  }
+  ChildPanels(
+    component.panels,
+    mainChild = { ArticlesList() },
+    detailsChild = { ArticleContent() },
+    animators = ChildPanelsAnimators(single = fade() + scale(), dual = fade() to fade()),
+  )
+}
+
+@OptIn(ExperimentalDecomposeApi::class)
+@Composable
+fun getPanelMode(): ChildPanelsMode {
+  val windowWidthSizeClass = currentWindowAdaptiveInfo().windowSizeClass.windowWidthSizeClass
+  val mode by remember {
+    derivedStateOf {
+      when (windowWidthSizeClass) {
+        WindowWidthSizeClass.COMPACT -> ChildPanelsMode.SINGLE
+        else -> ChildPanelsMode.DUAL
+      }
+    }
+  }
+  return mode
 }
