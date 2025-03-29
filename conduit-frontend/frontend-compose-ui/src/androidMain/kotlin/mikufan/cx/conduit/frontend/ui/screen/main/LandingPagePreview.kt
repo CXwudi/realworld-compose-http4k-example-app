dd8708582d5f9f3f055318593fff9e569f8f9130
CXwudi <cx2010017@gmail.com>
2025-03-29 16:50:47 -0400

    🎨 Add alert notification for landing page error
    Implement error handling with LaunchedEffect and alert notification, but Android app crashes due to unresolved issue

--- conduit-frontend/frontend-compose-ui/src/androidMain/kotlin/mikufan/cx/conduit/frontend/ui/screen/main/LandingPagePreview.kt
+++ conduit-frontend/frontend-compose-ui/src/androidMain/kotlin/mikufan/cx/conduit/frontend/ui/screen/main/LandingPagePreview.kt
@@ -2,10 +2,12 @@ package mikufan.cx.conduit.frontend.ui.screen.main
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import mikufan.cx.conduit.frontend.logic.component.landing.LandingPageComponent
 import mikufan.cx.conduit.frontend.logic.component.landing.LandingPageIntent
+import mikufan.cx.conduit.frontend.logic.component.landing.LandingPageLabel
 import mikufan.cx.conduit.frontend.logic.component.landing.LandingPageState
 import mikufan.cx.conduit.frontend.ui.screen.LandingPage
 import mikufan.cx.conduit.frontend.ui.util.SetupPreviewUI
@@ -20,6 +22,8 @@ fun LandingPagePreview() {
 
       override fun send(intent: LandingPageIntent) {
       }
+
+      override val labels: Flow<LandingPageLabel> = MutableStateFlow(LandingPageLabel.Failure("some error"))
     }
     LandingPage(mockComponent)
   }
--- conduit-frontend/frontend-compose-ui/src/commonMain/kotlin/mikufan/cx/conduit/frontend/ui/screen/LandingPage.kt
+++ conduit-frontend/frontend-compose-ui/src/commonMain/kotlin/mikufan/cx/conduit/frontend/ui/screen/LandingPage.kt
@@ -1,33 +1,28 @@
 package mikufan.cx.conduit.frontend.ui.screen
 
+import NotificationType
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.ime
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import createNotification
 import mikufan.cx.conduit.frontend.logic.component.landing.LandingPageComponent
 import mikufan.cx.conduit.frontend.logic.component.landing.LandingPageIntent
+import mikufan.cx.conduit.frontend.logic.component.landing.LandingPageLabel
 import mikufan.cx.conduit.frontend.ui.theme.LocalSpace
 
 @Composable
@@ -36,9 +31,17 @@ fun LandingPage(component: LandingPageComponent, modifier: Modifier = Modifier) 
   val state by component.state.collectAsState()
   val urlText = remember { derivedStateOf { state.url } }
 
-  val scope = rememberCoroutineScope()
-  component.labels
-
+  LaunchedEffect(Unit) {
+    component.labels.collect { label ->
+      when (label) {
+        is LandingPageLabel.Failure -> {
+          createNotification(NotificationType.ALERT).show(label.message)
+        }
+        // Handle other labels if necessary
+        else -> {}
+      }
+    }
+  }
 
   Box(
     contentAlignment = Alignment.Center,
@@ -54,35 +57,10 @@ fun LandingPage(component: LandingPageComponent, modifier: Modifier = Modifier) 
         onValueChange = { component.send(LandingPageIntent.TextChanged(it)) },
         singleLine = true,
       )
-//      AnimatedVisibility(
-//        visible = showError.value,
-//      ) {
-//        ErrorMessage(errMsg) // TODO: consider refactor to use https://github.com/KhubaibKhan4/Alert-KMP
-//      }
       Button(onClick = { component.send(LandingPageIntent.CheckAndMoveToMainPage) }) {
         Text("Connect")
       }
     }
 
-  }
-
-}
-
-@Composable
-private fun ColumnScope.ErrorMessage(message: State<String>, modifier: Modifier = Modifier) {
-  Row(
-    verticalAlignment = Alignment.CenterVertically,
-    modifier = Modifier.align(Alignment.Start)
-  ) {
-    Icon(
-      imageVector = Icons.Filled.Warning,
-      contentDescription = "Error",
-      tint = androidx.compose.ui.graphics.Color.Red
-    )
-    Spacer(modifier = Modifier.width(LocalSpace.current.horizontal.spacing))
-    Text(
-      text = message.value,
-      color = androidx.compose.ui.graphics.Color.Red
-    )
   }
 }
