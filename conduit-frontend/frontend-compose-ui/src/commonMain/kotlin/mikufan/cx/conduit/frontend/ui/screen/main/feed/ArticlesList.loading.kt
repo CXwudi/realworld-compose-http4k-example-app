547b18a0cf8a5cd7feb2c369ae25f6e46d82ca6e
CXwudi <cx2010017@gmail.com>
2025-04-20 18:30:48 -0400

    🎨 Make ArticlesListLoading modifier customizable
    Make loading screen more flexible by allowing parent composable to specify its size and other modifiers

--- conduit-frontend/frontend-compose-ui/src/commonMain/kotlin/mikufan/cx/conduit/frontend/ui/screen/main/feed/ArticlesList.loading.kt
+++ conduit-frontend/frontend-compose-ui/src/commonMain/kotlin/mikufan/cx/conduit/frontend/ui/screen/main/feed/ArticlesList.loading.kt
@@ -2,7 +2,6 @@ package mikufan.cx.conduit.frontend.ui.screen.main.feed
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,9 +12,9 @@ import mikufan.cx.conduit.frontend.ui.common.BouncingDotsLoading
 import mikufan.cx.conduit.frontend.ui.theme.LocalSpace
 
 @Composable
-fun ArticlesListLoading() {
+fun ArticlesListLoading(modifier: Modifier = Modifier) {
   Column(
-    modifier = Modifier.fillMaxSize(),
+    modifier = modifier,
     horizontalAlignment = Alignment.CenterHorizontally,
     verticalArrangement = Arrangement.spacedBy(LocalSpace.current.vertical.spacingLarge * 2, Alignment.CenterVertically)
   ) {
--- conduit-frontend/frontend-compose-ui/src/commonMain/kotlin/mikufan/cx/conduit/frontend/ui/screen/main/feed/ArticlesList.main.kt
+++ conduit-frontend/frontend-compose-ui/src/commonMain/kotlin/mikufan/cx/conduit/frontend/ui/screen/main/feed/ArticlesList.main.kt
@@ -1,10 +1,12 @@
 package mikufan.cx.conduit.frontend.ui.screen.main.feed
 
 import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import mikufan.cx.conduit.frontend.logic.component.main.feed.ArticlesListComponent
 import mikufan.cx.conduit.frontend.logic.component.main.feed.ArticlesListState
 
@@ -16,7 +18,7 @@ fun ArticlesList(component: ArticlesListComponent) {
 
   Crossfade(targetState = listState.value) {
     when (it) {
-      is ArticlesListState.Loading -> ArticlesListLoading()
+      is ArticlesListState.Loading -> ArticlesListLoading(Modifier.fillMaxSize())
       is ArticlesListState.Loaded -> {
         val collectedThumbInfosState = remember { derivedStateOf { it.collectedThumbInfos } }
         val isLoadingMoreState = remember { derivedStateOf { it.isLoadingMore } }
